d2997101b85b72c2da55d2a35a9180bcc65b4e37
Nagiko <n.t.woody.0914@gmail.com>
2021-02-06 20:03:43 +0900

    プロフィールアプリの完成

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+//    画像を表示する変数を作成
+    @IBOutlet var profileImageView: UIImageView!
+    
+//    ラベルを表示する変数を作成
+    @IBOutlet var profileCommentLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        画像を見えない状態にする
+        profileImageView.isHidden = true
+//        ラベルを見えない状態にする
+        profileCommentLabel.isHidden = true
+    }
+//    ボタンがタップされた時
+    @IBAction func tapButton1(){
+//        画像を見える状態にする
+        profileImageView.isHidden = false
+        
+//        ラベルを見える状態にする
+        profileCommentLabel.isHidden = false
     }
 
 
